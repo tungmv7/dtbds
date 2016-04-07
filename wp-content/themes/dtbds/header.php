6a01c1a9ab8bcf67d979424b578b08612ac4d9f5
Tung Mang Vien <tungmv7@gmail.com>
2016-04-07 20:48:29 +0700

    update seo plugins

--- wp-content/themes/dtbds/header.php
+++ wp-content/themes/dtbds/header.php
@@ -3,8 +3,6 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="">
-	<meta name="author" content="">
 
 	<!-- Bootstrap core CSS -->
 	<link href="<?= get_template_directory_uri() ?>/assets/css/bootstrap.min.css" rel="stylesheet">
