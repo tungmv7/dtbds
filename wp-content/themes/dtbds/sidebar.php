0399da4f37d25d1503ef72dbf43205be8b00241b
Tung Mang Vien <tungmv7@gmail.com>
2016-10-26 11:48:02 +0700

    sticky

--- wp-content/themes/dtbds/sidebar.php
+++ wp-content/themes/dtbds/sidebar.php
@@ -1,5 +1,7 @@
 <?= get_template_part('template-parts/search', 'box') ?>
 <?= get_template_part('template-parts/contact', 'box') ?>
+<div class="sticky">
 <?php dynamic_sidebar('ads-content-2') ?>
+</div>
 
 
--- wp-content/themes/dtbds/template-parts/contact-box.php
+++ wp-content/themes/dtbds/template-parts/contact-box.php
@@ -12,9 +12,9 @@
 
         <ul>
             <li><i class="fa fa-home"></i> <?= $contactData['congty']?></li>
-            <li><i class="fa fa-envelope"></i> <?= $contactData['email']?></li>
+            <li><i class="fa fa-envelope"></i> <a href="mailto:<?= $contactData['email']?>"><?= $contactData['email']?></a></li>
             <li><i class="fa fa-phone-square"></i> <?= $contactData['phone'] ?></li>
-            <li><i class="fa fa-facebook-square"></i> <?= $contactData['facebook'] ?></li>
+            <li style="white-space: nowrap;"><i class="fa fa-facebook-square"></i> <a href="<?= $contactData['facebook'] ?>" target="_blank"><?= $contactData['facebook'] ?></a></li>
             <li><i class="fa fa-share-square"></i> <?= $contactData['address'] ?></li>
         </ul>
     </div>
