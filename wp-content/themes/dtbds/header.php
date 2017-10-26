3e4d8c6cd724addbfbcbf4f258745cdbaf2da389
Tung Mang Vien <tungmv7@gmail.com>
2017-10-26 14:38:28 +0700

    add GA code

--- wp-content/themes/dtbds/header.php
+++ wp-content/themes/dtbds/header.php
@@ -26,6 +26,16 @@
 
 	<?php wp_head(); ?>
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108407182-2"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-108407182-2');
+    </script>
+
 </head>
 <body <?php body_class(); ?>>
 <div id="fb-root"></div>
